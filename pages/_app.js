8d33c334fd9489faa632d480df6a57f4d69a88a1
Ahmet Aygün <me@ahmetaygun.net>
2023-10-11 00:07:53 +0300

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import 'tailwindcss/tailwind.css'
+import { Analytics } from '@vercel/analytics/react'
 
 function AwesomeWeb3({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
 
 export default AwesomeWeb3
