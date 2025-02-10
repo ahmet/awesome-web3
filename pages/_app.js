8fd94d74c61e6f915253d68665c987e102287faa
Ahmet Aygün <me@ahmetaygun.net>
2025-02-10 23:56:07 +0300

    added vercel speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import 'tailwindcss/tailwind.css'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function AwesomeWeb3({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   )
 }
